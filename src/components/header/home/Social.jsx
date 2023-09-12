effc6f642ddedbd9266e0e03de7bf83c01f0bd4a
Chanisa jayawardhana <sadin.chanisa8@gmail.com>
2023-09-12 10:06:05 +0530

    Nav bars

---
+++ src/components/header/home/Social.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/cha_ni__________gah___/" className="home__social-icon" target='_blank'>
+            <i className='uil uil-instagram'></i>
+        </a>
+
+        <a href="https://github.com/chanisagithub" className="home__social-icon" target='_blank'>
+            <i className='uil uil-Github'></i>
+        </a>
+
+        <a href="https://discord.com/" className="home__social-icon" target='_blank'>
+            <i className='uil uil-discord'></i>
+            
+        </a>
+
+    </div>
+  )
+}
+
+export default Social
