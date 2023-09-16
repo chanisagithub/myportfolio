e33a54f5f150ee942b731dc151d422dc140b16cb
Chanisa jayawardhana <sadin.chanisa8@gmail.com>
2023-09-16 09:55:31 +0530

    Colors fixed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import "./about.css"
+import AboutImg from "../../assets/about.JPG"
+import CV from "../../assets/CV-Chanisa jayawardhana.pdf"
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className="about section" id="about">
+        <h2 className='section__title'>About Me</h2>
+        <span className="section__subtitle">My Introduction</span>
+
+        <div className="about__container container grid">
+            <img src={AboutImg} alt="" className="about__img" />
+
+            <div className="about__data"></div>
+        </div>
+    </section>
   )
 }
 
--- src/components/header/home/Data.jsx
+++ src/components/header/home/Data.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Data = () => {
     return (
         <div className="home__data">
-            <h1 className='home_title'>Chanisa Jay
+            <h1 className='home__title'>Chanisa Jay
                 <svg
                     width="36"
                     height="36"
