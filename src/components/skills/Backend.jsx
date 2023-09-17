215c1740dab41943315de0825b2b12eb247e1e10
Chanisa jayawardhana <sadin.chanisa8@gmail.com>
2023-09-17 20:12:37 +0530

    services added

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -48,7 +48,7 @@ const Backend = () => {
                 <i class='bx bx-badge-check'></i>
 
                 <div>
-                    <h3 className="skills__name">Firebase</h3>
+                    <h3 className="skills__name">Java</h3>
                     <span className="skills__level">Intermediate</span>
                 </div>
                 </div>
